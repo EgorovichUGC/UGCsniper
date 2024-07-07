0179a71c4fdd0e01a91b098050ca570297beb254
EgorovichUGC <egorsyulgin2016@gmail.com>
2024-07-07 13:25:22 +0300

    Update UGCsniperFree.lua

--- UGCsniperFree.lua
+++ UGCsniperFree.lua
@@ -158,11 +158,11 @@ end
 				local ClickingSpeed = 0
 				local loopActive = false
 
-				ugc:Toggle("Auto Cancel", false, function(bool)
+				main:Toggle("Auto Cancel", false, function(bool)
 
 					loopActive = bool
 
-					if loopActive then
+                                         if loopActive then
 						while loopActive == true do
 							local purchasePrompt = game.CoreGui:FindFirstChild("PurchasePrompt")
 							if purchasePrompt then
@@ -207,31 +207,26 @@ end
 				end)
 
 
-local actic = false
 
-ugc:Toggle("Auto Close Error", false, function(bool)
-	actic = bool
-	if actic then
-		while actic do
-			local pp = game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt")
-			if pp and pp.AlertContents and pp.AlertContents.Footer and pp.AlertContents.Footer.Buttons and not pp.AlertContents.Footer.Buttons:FindFirstChild("2") then
-				if pp.AlertContents.Footer.Buttons:FindFirstChild("1") then
-					local b1 = pp.AlertContents.Footer.Buttons[1].AbsolutePosition
-					game:GetService("VirtualInputManager"):SendMouseButtonEvent(b1.X + 55, b1.Y + 65.5, 0, true, game, 1)
-					wait()
-					game:GetService("VirtualInputManager"):SendMouseButtonEvent(b1.X + 55, b1.Y + 65.5, 0, false, game, 1)
-				end
-			end
-			wait()
-			
-			if actic == false then
-				break
-			end
-			
-		end
-	end
-end)
 
+
+				main:Toggle("Auto Close Error", false, function(bool)
+					loopActive = bool
+					spawn(function()
+						while bool == true do
+							local pp = game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt")
+							if pp and pp.AlertContents and pp.AlertContents.Footer and pp.AlertContents.Footer.Buttons and not pp.AlertContents.Footer.Buttons:FindFirstChild("1") then
+								if pp.AlertContents.Footer.Buttons:FindFirstChild("1") then
+									local b1 = pp.AlertContents.Footer.Buttons[1].AbsolutePosition
+									game:GetService("VirtualInputManager"):SendMouseButtonEvent(b1.X + 55, b1.Y + 65.5, 0, true, game, 1)
+									wait()
+									game:GetService("VirtualInputManager"):SendMouseButtonEvent(b1.X + 55, b1.Y + 65.5, 0, false, game, 1)
+								end
+							end
+							wait()
+						end
+					end)
+				end)
 				main:Toggle("Anti Afk", false, function(bool)
 					if bool == true then
 						Players.LocalPlayer.Idled:connect(function()
